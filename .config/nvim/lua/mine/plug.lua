54c6909f2ffd7d2cb31f82a185d169ae638992dd
Sergio <sergio@garciadelacruz.es>
2023-02-08 01:42:45 +0100

    Hover nvim

--- .config/nvim/lua/mine/plug.lua
+++ .config/nvim/lua/mine/plug.lua
@@ -17,7 +17,7 @@ return require('packer').startup(function(use)
 
 	use 'ggandor/leap.nvim'
 
-	use 'wellle/context.vim'
+	-- use 'wellle/context.vim'
 
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
@@ -81,5 +81,31 @@ return require('packer').startup(function(use)
 
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
+	use {
+		"lewis6991/hover.nvim",
+		config = function()
+			require("hover").setup {
+				init = function()
+					-- Require providers
+					require("hover.providers.lsp")
+					require('hover.providers.gh')
+					-- require('hover.providers.jira')
+					-- require('hover.providers.man')
+					-- require('hover.providers.dictionary')
+				end,
+				preview_opts = {
+					border = nil
+				},
+				-- Whether the contents of a currently open hover window should be moved
+				-- to a :h preview-window when pressing the hover keymap.
+				preview_window = false,
+				title = true
+			}
+
+			-- Setup keymaps
+			vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
+			vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
+		end
+	}
 	use {'arcticicestudio/nord-vim', as = 'nord'}
 end)
