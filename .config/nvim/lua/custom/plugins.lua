6f9e4b190830d5252dc43f1b03cbf11130be8108
sergio <sergio@garciadelacruz.es>
2023-08-12 16:29:05 +0200

    Nvim surround and fugitive

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -87,6 +87,20 @@ local plugins = {
       },
     },
   },
+  {
+    "tpope/vim-fugitive",
+    event = "VeryLazy"
+  },
+  {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  }
 
   -- To make a plugin not be loaded
   -- {
