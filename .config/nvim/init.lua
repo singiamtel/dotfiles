7f02eeacfc0db58def9e00fc15e37ed00f062d4e
Sergio Garcia <sergio@garciadelacruz.es>
2025-05-19 15:54:25 +0200

    Stylua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,142 +2,141 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
-    { "kevinhwang91/nvim-bqf", ft = "qf" },
-    "neovim/nvim-lspconfig",
-    'junegunn/fzf',
-    "nvim-treesitter/nvim-treesitter-context",
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        -- or                              , branch = '0.1.x',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{ "kevinhwang91/nvim-bqf", ft = "qf" },
+	"neovim/nvim-lspconfig",
+	"junegunn/fzf",
+	"nvim-treesitter/nvim-treesitter-context",
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		-- or                              , branch = '0.1.x',
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			-- "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		},
+		lazy = false,
+		branch = "regexp", -- This is the regexp branch, use this for the new version
+		config = function()
+			require("venv-selector").setup({
+				lazy = false,
+				settings = {
+					options = {
+						debug = true,
+						enable_cached_venvs = true,
+						cached_venv_automatic_activation = true,
+					},
+				},
+			})
+		end,
+	},
+	"shaunsingh/nord.nvim",
+	"github/copilot.vim",
+	"rcarriga/nvim-notify",
+	"tpope/vim-fugitive",
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {
+			disable_mouse = false,
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+				},
+				ensure_installed = {
+					"vimdoc",
+					"luadoc",
+					"vim",
+					"lua",
+					"markdown",
+				},
+			})
+		end,
+	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				suppressed_dirs = { "~/", "~/Downloads", "/" },
+			})
+		end,
+	},
+	{
+		"m00qek/baleia.nvim",
+		version = "*",
+		config = function()
+			vim.g.baleia = require("baleia").setup({})
 
-        }
-    },
-    {
-        "linux-cultist/venv-selector.nvim",
-        dependencies = {
-            "neovim/nvim-lspconfig", 
-            -- "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
-            { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-        },
-        lazy = false,
-        branch = "regexp", -- This is the regexp branch, use this for the new version
-        config = function()
-            require("venv-selector").setup {
-                lazy = false,
-                settings = {
-                    options = {
-                        debug = true,
-                        enable_cached_venvs = true,
-                        cached_venv_automatic_activation = true
-                    },
-                },
-            }
-        end
-    },
-    "shaunsingh/nord.nvim",
-    "github/copilot.vim",
-    'rcarriga/nvim-notify',
-    "tpope/vim-fugitive",
-    {
-       "m4xshen/hardtime.nvim",
-       lazy = false,
-       dependencies = { "MunifTanjim/nui.nvim" },
-       opts = {
-        disable_mouse = false,
-       },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                highlight = {
-                    enable = true,
-                },
-                ensure_installed = {
-                    "vimdoc",
-                    "luadoc",
-                    "vim",
-                    "lua",
-                    "markdown"
-                }
-            }
-        end,
-    },
-    {
-        "rmagatti/auto-session",
-        lazy = false,
-        config = function()
-            require("auto-session").setup {
-                log_level = "error",
-                suppressed_dirs = { "~/", "~/Downloads", "/" },
-            }
-        end,
-    },
-    {
-        "m00qek/baleia.nvim",
-        version = "*",
-        config = function()
-            vim.g.baleia = require("baleia").setup({ })
+			-- Command to colorize the current buffer
+			vim.api.nvim_create_user_command("Colorize", function()
+				vim.g.baleia.once(vim.api.nvim_get_current_buf())
+			end, { bang = true })
 
-            -- Command to colorize the current buffer
-            vim.api.nvim_create_user_command("Colorize", function()
-                vim.g.baleia.once(vim.api.nvim_get_current_buf())
-            end, { bang = true })
-
-            -- Command to show logs 
-            vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
-        end,
-    },
-    "Almo7aya/openingh.nvim",
-    "tpope/vim-surround",
-    'metakirby5/codi.vim'
+			-- Command to show logs
+			vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+		end,
+	},
+	"Almo7aya/openingh.nvim",
+	"tpope/vim-surround",
+	"metakirby5/codi.vim",
 })
 
 vim.notify = require("notify")
 
-require('keymaps').setup()
-
+require("keymaps").setup()
 
 -- options
-vim.opt.clipboard:append { 'unnamedplus' }
+vim.opt.clipboard:append({ "unnamedplus" })
 vim.opt.ignorecase = true
 vim.opt.hlsearch = false
 vim.opt.smartcase = true
-vim.opt.undodir = os.getenv( "HOME" ) .. '/.local/share/nvim/undo' -- -_-
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undo" -- -_-
 vim.opt.undofile = true
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 vim.o.number = true
 vim.o.relativenumber = true
 vim.g.nord_contrast = true
-vim.cmd [[autocmd FileType * set formatoptions-=ro]] -- otherwise it gets overwritten by some random ftplugin. absolutely bullshit
+vim.cmd([[autocmd FileType * set formatoptions-=ro]]) -- otherwise it gets overwritten by some random ftplugin. absolutely bullshit
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -148,33 +147,34 @@ vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.list = true
 vim.opt.listchars = {
-    tab = '!·',
-    trail = '·'
+	tab = "!·",
+	trail = "·",
 }
 
-vim.cmd [[autocmd FileType python setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab]]
+vim.cmd([[autocmd FileType python setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab]])
 
-vim.cmd[[colorscheme nord]]
+vim.cmd([[colorscheme nord]])
 
 vim.o.guifont = "FiraCode Nerd Font Propo:h12"
 
 -- lsp
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
-local servers = { "html", "cssls", "ts_ls", "clangd", "eslint", "rust_analyzer", "tailwindcss", "terraform_lsp", "bashls" }
+local servers =
+	{ "html", "cssls", "ts_ls", "clangd", "eslint", "rust_analyzer", "tailwindcss", "terraform_lsp", "bashls" }
 
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-vim.lsp.enable({"pyrefly"})
+vim.lsp.enable({ "pyrefly" })
 
-lspconfig.terraformls.setup {
-    on_attach = function()
-        require("treesitter-terraform-doc").setup()
-    end,
-    capabilities = capabilities,
-}
+lspconfig.terraformls.setup({
+	on_attach = function()
+		require("treesitter-terraform-doc").setup()
+	end,
+	capabilities = capabilities,
+})
--- .config/nvim/keymaps.lua
+++ .config/nvim/keymaps.lua
@@ -1,74 +1,73 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-local function noop() end  -- FIXME: There must be a better way to disable the help popup
+local function noop() end -- FIXME: There must be a better way to disable the help popup
 
 local function toggle_quickfix()
-    local qf_exists = false
-    for _, win in pairs(vim.fn.getwininfo()) do
-        if win["quickfix"] == 1 then
-            qf_exists = true
-        end
-    end
-    
-    if qf_exists then
-        vim.cmd("cclose")
-    elseif not vim.tbl_isempty(vim.fn.getqflist()) then
-        vim.cmd("copen")
-    end
+	local qf_exists = false
+	for _, win in pairs(vim.fn.getwininfo()) do
+		if win["quickfix"] == 1 then
+			qf_exists = true
+		end
+	end
+
+	if qf_exists then
+		vim.cmd("cclose")
+	elseif not vim.tbl_isempty(vim.fn.getqflist()) then
+		vim.cmd("copen")
+	end
 end
 
 local M = {}
 
 M.setup = function()
-    -- Buffer management
-    vim.keymap.set('n', '<leader>bd', "<cmd>bd<cr>", {desc = "Delete buffer"})
-    vim.keymap.set('n', '<leader>bq', "<cmd>q<cr>", {desc = "Quit"})
-    vim.keymap.set('n', '<leader><leader>', "<cmd>b#<cr>", {desc = "Switch to last buffer"})
-    vim.keymap.set('n', '<c-j>', "<cmd>bn<cr>", {desc = "Next buffer"})
-    vim.keymap.set('n', '<c-k>', "<cmd>bp<cr>", {desc = "Previous buffer"})
+	-- Buffer management
+	vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete buffer" })
+	vim.keymap.set("n", "<leader>bq", "<cmd>q<cr>", { desc = "Quit" })
+	vim.keymap.set("n", "<leader><leader>", "<cmd>b#<cr>", { desc = "Switch to last buffer" })
+	vim.keymap.set("n", "<c-j>", "<cmd>bn<cr>", { desc = "Next buffer" })
+	vim.keymap.set("n", "<c-k>", "<cmd>bp<cr>", { desc = "Previous buffer" })
 
-    -- Editor functions
-    vim.keymap.set('n', '<F1>', noop, {desc = "Disable help popup"})
-    vim.keymap.set('n', '<leader>dt', "<cmd>:%s/\\s\\+$//e<cr>", {desc = "Delete trailing spaces"})
-    vim.keymap.set('n', '<leader>h', "<cmd>set hls!<cr>", {desc = "Toggle search highlighting"})
-    vim.keymap.set('n', '<down>', '<c-e>', { silent = true })
-    vim.keymap.set('n', '<up>', '<c-y>', { silent = true })
-    vim.keymap.set('n', 'n', 'nzzzv')
-    vim.keymap.set('n', 'N', 'Nzzzv')
+	-- Editor functions
+	vim.keymap.set("n", "<F1>", noop, { desc = "Disable help popup" })
+	vim.keymap.set("n", "<leader>dt", "<cmd>:%s/\\s\\+$//e<cr>", { desc = "Delete trailing spaces" })
+	vim.keymap.set("n", "<leader>h", "<cmd>set hls!<cr>", { desc = "Toggle search highlighting" })
+	vim.keymap.set("n", "<down>", "<c-e>", { silent = true })
+	vim.keymap.set("n", "<up>", "<c-y>", { silent = true })
+	vim.keymap.set("n", "n", "nzzzv")
+	vim.keymap.set("n", "N", "Nzzzv")
 
-    -- Git commands
-    vim.keymap.set('n', '<leader>gb', "<cmd>G blame -w<cr>", {desc = "Git blame"})
-    vim.keymap.set('n', '<leader>gd', "<cmd>Gvdiffsplit<cr>", {desc = "Git diff split"})
+	-- Git commands
+	vim.keymap.set("n", "<leader>gb", "<cmd>G blame -w<cr>", { desc = "Git blame" })
+	vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<cr>", { desc = "Git diff split" })
 
-    -- Quickfix navigation
-    vim.keymap.set('n', '<leader>j', "<cmd>cprevious<cr>", {desc = "Previous quickfix item"})
-    vim.keymap.set('n', '<leader>k', "<cmd>cnext<cr>", {desc = "Next quickfix item"})
-    vim.keymap.set('n', '<leader>q', toggle_quickfix, {desc = "Toggle quickfix window"})
+	-- Quickfix navigation
+	vim.keymap.set("n", "<leader>j", "<cmd>cprevious<cr>", { desc = "Previous quickfix item" })
+	vim.keymap.set("n", "<leader>k", "<cmd>cnext<cr>", { desc = "Next quickfix item" })
+	vim.keymap.set("n", "<leader>q", toggle_quickfix, { desc = "Toggle quickfix window" })
 
-    -- LSP keymaps
-    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, {desc = "LSP: Rename symbol"})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {desc = "LSP: Go to definition"})
-    vim.keymap.set('n', '<s-I>', vim.diagnostic.open_float, {desc = "LSP: Show diagnostics"})
-    vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next, {desc = "LSP: Jump to next diagnostic"})
-    vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev, {desc = "LSP: Jump to previous diagnostic"})
-    vim.keymap.set('n', '<leader>li', "<cmd>LspInfo<cr>", {desc = "LSP: Show info"})
-    vim.keymap.set('n', '<leader>lk', "<cmd>LspStop<cr>", {desc = "LSP: Stop"})
-    vim.keymap.set('n', '<leader>ls', "<cmd>LspStart<cr>", {desc = "LSP: Start"})
-    vim.keymap.set('n', '<leader>v', "<cmd>VenvSelect<cr>", {desc = "Select Python virtual environment"})
+	-- LSP keymaps
+	vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
+	vim.keymap.set("n", "<s-I>", vim.diagnostic.open_float, { desc = "LSP: Show diagnostics" })
+	vim.keymap.set("n", "<leader>en", vim.diagnostic.goto_next, { desc = "LSP: Jump to next diagnostic" })
+	vim.keymap.set("n", "<leader>ep", vim.diagnostic.goto_prev, { desc = "LSP: Jump to previous diagnostic" })
+	vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "LSP: Show info" })
+	vim.keymap.set("n", "<leader>lk", "<cmd>LspStop<cr>", { desc = "LSP: Stop" })
+	vim.keymap.set("n", "<leader>ls", "<cmd>LspStart<cr>", { desc = "LSP: Start" })
+	vim.keymap.set("n", "<leader>v", "<cmd>VenvSelect<cr>", { desc = "Select Python virtual environment" })
 
-    -- Telescope keymaps
-    local telescope = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ea', telescope.diagnostics, {desc = "Telescope: Diagnostics"})
-    vim.keymap.set('n', '<leader>ff', function()
-        telescope.find_files({
-            hidden = true,
-            file_ignore_patterns = { "^.git/" }
-        })
-    end, {desc = "Telescope: Find files"})
-    vim.keymap.set('n', '<leader>fw', telescope.live_grep, {desc = "Telescope: Live grep"})
-    vim.keymap.set('n', '<leader>fb', telescope.buffers, {desc = "Telescope: Buffers"})
-    vim.keymap.set('n', '<leader>fh', telescope.help_tags, {desc = "Telescope: Help tags"})
+	-- Telescope keymaps
+	local telescope = require("telescope.builtin")
+	vim.keymap.set("n", "<leader>ea", telescope.diagnostics, { desc = "Telescope: Diagnostics" })
+	vim.keymap.set("n", "<leader>ff", function()
+		telescope.find_files({
+			hidden = true,
+			file_ignore_patterns = { "^.git/" },
+		})
+	end, { desc = "Telescope: Find files" })
+	vim.keymap.set("n", "<leader>fw", telescope.live_grep, { desc = "Telescope: Live grep" })
+	vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Telescope: Buffers" })
+	vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Telescope: Help tags" })
 end
 
 return M
-
